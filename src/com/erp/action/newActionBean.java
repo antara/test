47dcdd488d39e08cafb1a13004b9d829b4254735
Antara <antara@revolutrix.com>
2012-06-11 12:05:07 +0530

    new action bean just 4 learling.

--- src/com/erp/action/newActionBean.java
+++ src/com/erp/action/newActionBean.java
@@ -7,5 +7,14 @@ package com.erp.action;
  * Time: 11:54:38 AM
  * To change this template use File | Settings | File Templates.
  */
-public class newActionBean {
+public class newActionBean extends BaseActionBean{
+    String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
 }
